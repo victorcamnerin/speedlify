bdfa6aabe30c1600384a38eba33e2f9c5869fab8
Victor Camnerin <victorcamnerin@gmail.com>
2020-08-27 21:40:43 +0200

    Update and rename sample.js to cmnrn-sites.js

---
+++ _data/sites/cmnrn-sites.js
@@ -0,0 +1,12 @@
+module.exports = {
+	name: "CMNRN", // optional, falls back to object key
+	description: "Site created by the development and design company CMNRN",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://victorcamnerin.com/",
+		"https://oskarlindwall.se",
+		"https://musika-blasexperten.se"
+	]
+};
